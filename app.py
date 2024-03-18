690baa6b5b903eb40a1b0652df7d1585ce766f6d
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2024-03-18 07:59:33 -0500

    Primer

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketIO = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketIO.on('mensaje')
+def handle_mensaje(msg):
+    print(msg)
+    socketIO.emit('mensaje' , msg)
+
+if __name__ == "__main__":
+    socketIO.run(app, debug = True)
